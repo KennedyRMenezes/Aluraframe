946b83ef7f2ab7f68e69d4f687e2ecb021a2f7b2
KennedyRMenezes <54713511+KennedyRMenezes@users.noreply.github.com>
2020-04-21 12:58:03 -0300

    JavaScript Avançado I: ES6: 2x8

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,7 +2,7 @@ class Negociacao { // toda classe c/ "contructor" só pode ser chamada em outro 
 
 	constructor (data, quantidade, valor) { // constructor é uma espécie de função
 
-		this._data = data;
+		this._data = new Date(data.getTime());
 		this._quantidade = quantidade;
 		this._valor = valor;
 		Object.freeze(this);
@@ -14,7 +14,7 @@ class Negociacao { // toda classe c/ "contructor" só pode ser chamada em outro 
 	}  
 
 	get data() {
-		return this._data;
+		return new Date(this._data.getTime());
 	}
 
 	get quantidade() {
