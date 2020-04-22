73efe07e03bc30dfb193d1409ecadccf9ee77bfc
KennedyRMenezes <54713511+KennedyRMenezes@users.noreply.github.com>
2020-04-22 20:36:07 -0300

    JavaScript Avançado I: ES6: 4x3

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,14 +1,18 @@
 class DateHelper {
 
-	dataParaTexto(data) {
+	constructor () {
+		throw new Error('Esta não pode ser instanciada')
+	}
+
+	static dataParaTexto(data) {
 
 		return data.getDate() // arrumando a data para aparecer dia/mês/ano
 		+ "/" + (data.getMonth() + 1)
 		+ '/' + data.getFullYear();
 
 	}
-	
-	textoParaData(texto) {
+
+	static textoParaData(texto) {
 		return new Date(...texto.split('-').map((item, indice) => item - indice % 2));// pode ser usado com uma expressão regular --> .replace(/-/g, ',')
 	}
 
