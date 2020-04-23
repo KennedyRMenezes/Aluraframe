3dd725140a521724e2e24f7265ce9e38d0883989
KennedyRMenezes <54713511+KennedyRMenezes@users.noreply.github.com>
2020-04-22 21:26:41 -0300

    JavaScript Avançado I: ES6: 4x4

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -6,15 +6,21 @@ class DateHelper {
 
 	static dataParaTexto(data) {
 
-		return data.getDate() // arrumando a data para aparecer dia/mês/ano
-		+ "/" + (data.getMonth() + 1)
-		+ '/' + data.getFullYear();
-
+		return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`; // arrumando a data para aparecer dia/mês/ano
+		
 	}
 
 	static textoParaData(texto) {
+
+		if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+			throw new Error ('Deve estar no formato aaaa-mm-dd');
+
 		return new Date(...texto.split('-').map((item, indice) => item - indice % 2));// pode ser usado com uma expressão regular --> .replace(/-/g, ',')
 	}
 
 
 }
+
+// data.getDate() // arrumando a data para aparecer dia/mês/ano
+	//	+ "/" + (data.getMonth() + 1)
+	//	+ '/' + data.getFullYear();
