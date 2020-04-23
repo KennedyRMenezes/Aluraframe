9d6a2f46b0016dbc9852c2d25914fd58d0afa9cc
KennedyRMenezes <54713511+KennedyRMenezes@users.noreply.github.com>
2020-04-23 17:13:55 -0300

    JavaScript Avançado I: ES6: 5x4

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -17,9 +17,8 @@ class NegociacoesView {
 			    </thead>
 			        
 			    <tbody>
-			    	${model.negociacoes.map(n => {
-
-			    		return `
+			    	${model.negociacoes.map(n => 
+			    		 `
 			    			<tr>
 			    				<td>${DateHelper.dataParaTexto(n.data)}</td>
 			    				<td>${n.quantidade}</td>
@@ -27,10 +26,18 @@ class NegociacoesView {
 			    				<td>${n.volume}</td>
 			    			</tr>
 			    		`
-			    	}).join('')}
+			    	).join('')}
 			    </tbody>
 			        
 			    <tfoot>
+			    	<td colspan="3"></td>
+			    	<td>${
+			    		(function(){
+			    			let total = 0;
+			    			model.negociacoes.forEach(n => total += n.volume);
+			    			return total;
+			    		})()
+			    	}</td>
 			    </tfoot>
 			</table>
 		`; 
